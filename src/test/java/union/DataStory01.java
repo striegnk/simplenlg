92c7690893f9d2c95b7de25ea673bef58fc6bbf9
Ashvin <gokhoolb@union.edu>
2017-08-10 21:43:04 -0400

    Code for generation of first sentence

--- src/test/java/union/DataStory01.java
+++ src/test/java/union/DataStory01.java
@@ -1,8 +1,16 @@
 package union;
 
+import simplenlg.features.Feature;
+import simplenlg.features.NumberAgreement;
 import simplenlg.framework.*;
 import simplenlg.lexicon.*;
+import simplenlg.phrasespec.NPPhraseSpec;
+import simplenlg.phrasespec.PPPhraseSpec;
+import simplenlg.phrasespec.SPhraseSpec;
+import simplenlg.phrasespec.VPPhraseSpec;
 import simplenlg.realiser.english.*;
+
+import java.awt.datatransfer.FlavorEvent;
 
 /**
  * The story:
@@ -58,6 +66,63 @@ public class DataStory01 {
      * cylinders.
      **/
     private void sentence1() {
+        SPhraseSpec sentence = nlgFactory.createClause();
+
+        NPPhraseSpec sub1 = nlgFactory.createNounPhrase("this", "dataset");
+        VPPhraseSpec verb1 = nlgFactory.createVerbPhrase("contain");
+        NPPhraseSpec obj1 = nlgFactory.createNounPhrase("information");
+        PPPhraseSpec prep1 = nlgFactory.createPrepositionPhrase("about", "different types of cars");
+        obj1.addComplement(prep1);
+
+        NPPhraseSpec fuelEco = nlgFactory.createNounPhrase("fuel economy");
+        NPPhraseSpec car =nlgFactory.createNounPhrase("the", "car");
+        car.setFeature(Feature.NUMBER, NumberAgreement.PLURAL);
+        car.setFeature(Feature.POSSESSIVE, true);
+        fuelEco.setSpecifier(car);
+        SPhraseSpec s1 = nlgFactory.createClause();
+        s1.setFrontModifier("such as");
+        s1.setObject(fuelEco);
+        s1.setFeature(Feature.APPOSITIVE, true);
+
+        PPPhraseSpec indObj1 = nlgFactory.createPrepositionPhrase("to", "accelerate from 0 to 60 mph");
+        VPPhraseSpec verb2 = nlgFactory.createVerbPhrase("take");
+        WordElement sub2 = (WordElement) nlgFactory.createWord("it", LexicalCategory.PRONOUN);
+        WordElement obj2 = (WordElement) nlgFactory.createWord("them",  LexicalCategory.PRONOUN);
+        SPhraseSpec s2 = nlgFactory.createClause();
+        s2.setFrontModifier("how long");
+        s2.setSubject(sub2);
+        s2.setVerb(verb2);
+        s2.setObject(obj2);
+        s2.setPostModifier(indObj1);
+        s2.setFeature(Feature.APPOSITIVE, true);
+
+        NPPhraseSpec s3 = nlgFactory.createNounPhrase("their", "weight");
+        s3.setFeature(Feature.APPOSITIVE, true);
+
+        NPPhraseSpec engine = nlgFactory.createNounPhrase("their engine");
+        engine.setFeature(Feature.POSSESSIVE, true);
+        NPPhraseSpec power = nlgFactory.createNounPhrase("power");
+        power.setSpecifier(engine);
+        NPPhraseSpec disp = nlgFactory.createNounPhrase("displacement");
+        NPPhraseSpec numCyl = nlgFactory.createNounPhrase("number of cylinders");
+        CoordinatedPhraseElement obj4 = nlgFactory.createCoordinatedPhrase(power, disp);
+        obj4.addCoordinate(numCyl);
+        SPhraseSpec s4 = nlgFactory.createClause();
+        s4.setSubject(obj4);
+
+        CoordinatedPhraseElement object = nlgFactory.createCoordinatedPhrase(s1, s2);
+        object.addCoordinate(s3);
+        object.addCoordinate(s4);
+
+        sentence.setSubject(sub1);
+        sentence.setVerb(verb1);
+        sentence.setObject(obj1);
+        sentence.addPostModifier(object);
+
+        String output = realiser.realiseSentence(sentence);
+        System.out.println(output);
+
+
 
     }
 
